8af955d6e7830dff332e04fcb8480ef8d260a55f
Adam Avramov <adam@hack.bg>
2021-08-11 11:07:35 +0300

    neovim: add vim-vue

--- neovim/default.nix
+++ neovim/default.nix
@@ -33,7 +33,7 @@ let
     vim-tmux vim-tmux-navigator
     bufexplorer airline
     /*languages*/
-    tabular vim-markdown rust-vim vim-nix
+    tabular vim-markdown rust-vim vim-nix vim-vue
     vim-pug vim-toml vim-glsl plantuml-syntax
     stylus cypher solidity
     /*themes*/
