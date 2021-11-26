d53f1c6bc354206e261c0620ac0f1f4d6dbd42b9
Adam A <adam@hack.bg>
2021-11-26 17:40:41 +0200

    rust: pin to hackbg fork of nixpkgs-mozilla

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ self: super: {
     pkgs = import <nixpkgs> {
       overlays = [
         (import (builtins.fetchTarball {
-          url    = "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz";
+          url    = "https://github.com/hackbg/nixpkgs-mozilla/archive/master.tar.gz";
           sha256 = "1kapba23cy3y1ghyqpm0rapbrfj3qdkd7v58c6g8bhpys9qq1aic";
         }))
       ];
