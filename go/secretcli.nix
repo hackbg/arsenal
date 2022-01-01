ba3b1442ed885bee34f3ec0c1a0287c1b7c8f773
Adam A <adam@hack.bg>
2022-01-01 03:15:36 +0200

    go/secretcli: support both Linux and macOS

--- go/secretcli.nix
+++ go/secretcli.nix
@@ -1,22 +1,25 @@
-{pkgs ? import <nixpkgs> {}, ...}:let
-  repo    = "enigmampc/SecretNetwork";
+{ pkgs               ? import <nixpkgs> {}
+, patchGitHubRelease ? (import ../lib/patchGitHubRelease.nix) pkgs
+, forPlatform        ? (import ../lib/dispatchPlatform.nix)   pkgs
+, ...}:
+
+let
+
+  name    = "secretcli";
   version = "v1.0.4";
-  system  = pkgs.stdenv.hostPlatform.system;
-  binary  = if system == "x86_64-linux"  then "secretcli-linux-amd64"       else
-            if system == "x86_64-darwin" then "secretcli-darwin-10.6-amd64" else
-            "unsupported_platform";
-in pkgs.stdenv.mkDerivation {
-  name =
-    "secretcli-${version}";
-  src =
-    pkgs.fetchurl {
-      url    = "https://github.com/${repo}/releases/download/${version}/${binary}";
+  platform = forPlatform {
+    "x86_64-linux" = {
+      binary = "secretcli-linux-amd64";
+      sha256 = "1mlrns9d52ill3fn00fdxmp4r0lmmffz1w8qwpw7q1ac6y35ma8k";
+    };
+    "x86_64-darwin" = {
+      binary = "secretcli-darwin-10.6-amd64";
       sha256 = "sha256-e/jGFTrafSXapOoHLJfPKwfxXRSReIDyB9ODU2Z86Jk=";
     };
-  nativeBuildInputs =
-    with pkgs; [ autoPatchelfHook ];
-  phases =
-    [ "patchPhase" "installPhase" ];
-  installPhase =
-    ''ls -al; install -m755 -D $src $out/bin/secretcli'';
-}
+  };
+
+in (patchGitHubRelease
+  "${name}-${version}"
+  "https://github.com/enigmampc/SecretNetwork/releases/download/v${version}/${platform.binary}"
+  platform.sha256
+  ''ls -al; install -m755 -D $src $out/bin/secretcli'')
