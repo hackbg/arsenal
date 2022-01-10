5e88ce440d433f3bdce88a1f10c54bbdda84ea54
Adam A <adam@hack.bg>
2022-01-11 00:14:41 +0200

    neovim: add nightly overlay

--- default.nix
+++ default.nix
@@ -2,10 +2,16 @@ self: super: {
   hackbg = import ./pkgs.nix {
     pkgs = import <nixpkgs> {
       overlays = [
+
         (import (builtins.fetchTarball {
           url    = "https://github.com/hackbg/nixpkgs-mozilla/archive/master.tar.gz";
           sha256 = "1kapba23cy3y1ghyqpm0rapbrfj3qdkd7v58c6g8bhpys9qq1aic";
         }))
+
+        (import (builtins.fetchTarball {
+          url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+        }))
+
       ];
     };
   };
