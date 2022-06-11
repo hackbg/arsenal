69dc828e4097ac1ea01e69952c17d5cec93de52a
Adam A <adam@hack.bg>
2022-06-11 23:03:21 +0300

    bash: remove colors from prompt, make like pravetz 8

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,6 +1,6 @@
 export PS1=
-CATS=(😺 😸 😹 😻 😼 😽 🙀 😿 😾 ⚔️  ⚽ 🎩 🐰 🌙 )
-CAT=${CATS[$RANDOM % ${#CATS[@]}]}
+#CATS=(😺 😸 😹 😻 😼 😽 🙀 😿 😾 ⚔️  ⚽ 🎩 🐰 🌙 )
+#CAT=${CATS[$RANDOM % ${#CATS[@]}]}
 function success_indicator() {
   if [ $? -eq 0 ] ; then echo "🟢"; else echo "🔴 $?"; fi }
-export PS1='$(success_indicator)\n\n\n$CAT \e[0;35m⬢ \w\e[0m '
+export PS1='$(success_indicator)\n\n\n\w ] '
