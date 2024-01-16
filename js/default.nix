852f27246373ac0db08d9177250ab5428ba82896
Adam A <adam@hack.bg>
2024-01-16 19:22:31 +0200

    js: just use nodejs_latest

--- js/default.nix
+++ js/default.nix
@@ -2,7 +2,10 @@
   name = "hackbg-arsenal-javascript";
   paths = with pkgs; [
     #nodejs-14_x
-    nodejs-18_x
+    #nodejs-16_x
+    #nodejs-18_x
+    #nodejs-20
+    nodejs_latest
     nodePackages.pnpm
     nodePackages.yarn
     python310 # for gyp
