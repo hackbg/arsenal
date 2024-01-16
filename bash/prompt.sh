a502ef3594fc8020018c732b0e789d9a96c2af4d
Adam A <adam@hack.bg>
2024-01-16 19:21:18 +0200

    bash: remove emojis from prompt 😿
    can mess up cursor position in some terminals/modes

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -3,4 +3,4 @@ CATS=(😺 😸 😹 😻 😼 😽 🙀 😿 😾 ⚔️  ⚽ 🎩 🐰 🌙 )
 CAT=${CATS[$RANDOM % ${#CATS[@]}]}
 function success_indicator() {
   if [ $? -eq 0 ] ; then echo "🟢"; else echo "🔴 $?"; fi }
-export PS1='$(success_indicator)\n\n\n\w $CAT '
+export PS1='$(success_indicator)\n\n\n\w '
