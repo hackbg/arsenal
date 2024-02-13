a5da8f82aadf0b90175eace3ed96319c6dd9d5dd
Adam A <adam@hack.bg>
2024-02-13 15:16:58 +0200

    feat: add default.nix for use as nixpkgs

--- default.nix
+++ default.nix
@@ -1,3 +1,5 @@
-pkgs: oldPkgs: with oldPkgs.lib; let
-  overlays = (map import (import ./overlays.nix));
-in (foldl' (flip extends) (_: oldPkgs) overlays) pkgs
+import <nixpkgs> { overlays = import ./overlays.nix; }
+
+#pkgs: oldPkgs: with oldPkgs.lib; let
+  #overlays = (map import (import ./overlays.nix));
+#in (foldl' (flip extends) (_: oldPkgs) overlays) pkgs
