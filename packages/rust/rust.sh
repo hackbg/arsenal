22c33d936a4a72b9d97bd7c4866467c4a229d9b1
Adam A <adam@hack.bg>
2024-02-13 22:43:34 +0200

    fix(rust): don't export RUSTFLAGS

--- packages/rust/rust.sh
+++ packages/rust/rust.sh
@@ -5,5 +5,5 @@
 #export RUSTFLAGS="$RUSTFLAGS -Zinstrument-coverage"
 #cargo install grcov
 export RUST_BACKTRACE=1
-export RUSTFLAGS="-Zmacro-backtrace"
+#export RUSTFLAGS="-Zmacro-backtrace"
 export PATH="$PATH:$HOME/.cargo/bin"
